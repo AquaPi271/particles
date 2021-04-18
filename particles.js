70538f0bcbcc9856cb05685e99f771b24636e46f
Tim Ozimek <tiozimek@microsoft.com>
2021-04-18 18:07:38 -0400

    Single particle bigger.

--- particles.js
+++ particles.js
@@ -105,7 +105,7 @@ function setup_shaders( ) {
         attribute vec3 vertex_position;
 
         void main(void) {
-            gl_PointSize = 2.0;
+            gl_PointSize = 3.0;
             gl_Position = vec4(vertex_position,1.0);
         }
     `;
